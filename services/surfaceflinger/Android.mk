4552ee449be511af5e578f8f0503066b65a68fb9
Arun Kumar K.R <akumarkr@codeaurora.org>
2015-12-02 18:19:20 -0800

    SurfaceFlinger: Move Extended SF classes to same repository
    Move the ExSurfaceFlinger, ExLayer, etc., classes to this
    repository, to avoid dependency accross projects with static
    linking
    
    The following commits are included in this:
    commit 04f501b31f627e47ae540b5160b2f86b7adbd921
    Author: Baldev Sahu <bsahu@codeaurora.org>
    Date:   Wed Nov 25 18:05:51 2015 +0530
    
    display-fw: Properly initialise mIsGPUAllowedForProtected flag
    
    - This flag need to be initialize to false when layer is
      created.
    
    CRs-fixed: 942102
    
    commit 09032188751356db295bc5467133ec484dc536cd
    Author: Arun Kumar K.R <akumarkr@codeaurora.org>
    Date:   Tue Oct 20 10:22:51 2015 -0700
    
    display-fw: Enable checking for INTERNAL_ONLY private flag
    
    Check for INTERNAL_ONLY private flag and return correct values
    from the function isIntOnly
    
    CRs-fixed: 927880
    
    commit f687ac25b3482b92da3c252d29b65e850cca3ce1
    Author: Arun Kumar K.R <akumarkr@codeaurora.org>
    Date:   Thu Oct 15 16:37:11 2015 -0700
    
    display-frameworks: Include libqdutils
    only for target builds
    
    Featurize including libqdutils for targets which uses QCOM_BSP,
    helps in resolving compilation for emulator builds
    
    commit 4ec765e01be506a8083980fc746a1737bbac4bf2
    Author: Arun Kumar K.R <akumarkr@codeaurora.org>
    Date:   Wed Oct 7 18:33:34 2015 -0700
    
    display-fw: Disable rotation animation on secondary displays
    
    - Set the animating flag on layer during animation which is used
      as hint to disable rotation animation on secondary displays.
    - Set sys.disable_ext_animation to enable the feature
    - Remove unused definitions in the extended hwc header
    
    CRs-fixed: 922304
    
    commit 6998bbdfd7af3126b64897b16b6961e84952601b
    Author: Baldev Sahu <bsahu@codeaurora.org>
    Date:   Thu Jul 30 16:01:15 2015 +0530
    
    display-frameworks: Extend support for MDP3
    
    - Add support for HWC_BLIT
    - Allow GPU to compose widevine level 3
    
    commit 2fa7aa1684c0162b8226c4327c86c6eaf6b76101
    Author: Manoj Kumar <manoj@codeaurora.org>
    Date:   Thu Aug 20 13:52:15 2015 -0700
    
    display-frameworks: Override mOutputUsage with sink Usage flags
    
    mOutputUsage flags contain default usage flags and doesnt reflect
    sink usage flags. Override mOutputUsage with sink usage flags.
    
    This change is needed to support WFD Secure usecase.
    
    commit 09da768794c8c6acd786d73239ed5a739695b8cb
    Author: Raj Kamal <rkamal@codeaurora.org>
    Date:   Mon Jul 20 10:46:27 2015 +0530
    
    display-frameworks: Extend the classes defined in SF
    
    Add support for custom classes ExSurfaceFlinger,
    ExLayer, ExHWComposer and ExVirtualDisplaySurface
    
    Change-Id: I205a3051f810cb6e6869954b87995b6eddf645d3

--- services/surfaceflinger/Android.mk
+++ services/surfaceflinger/Android.mk
@@ -106,13 +106,16 @@ LOCAL_SHARED_LIBRARIES := \
     libgui \
     libpowermanager
 
-
 ifeq ($(TARGET_USES_QCOM_BSP), true)
-    LOCAL_C_INCLUDES += hardware/qcom/display/libgralloc
-    LOCAL_C_INCLUDES += hardware/qcom/display/libqdutils
-    LOCAL_C_INCLUDES += vendor/qcom/opensource/display-frameworks/include
+    LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libgralloc
+    LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libqdutils
     LOCAL_SHARED_LIBRARIES += libqdutils
     LOCAL_CFLAGS += -DQTI_BSP
+    LOCAL_SRC_FILES += \
+        ExSurfaceFlinger/ExLayer.cpp \
+        ExSurfaceFlinger/ExSurfaceFlinger.cpp \
+        ExSurfaceFlinger/ExVirtualDisplaySurface.cpp \
+        ExSurfaceFlinger/ExHWComposer.cpp
 endif
 
 LOCAL_MODULE := libsurfaceflinger
